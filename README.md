4bae9e6797f2be67466dd7e6931bf47db6bd9b24
Inbar Artzi <i.artzi123@gmail.com>
2023-11-09 13:09:06 -0500

    skeleton of csv parser

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 320ProjectBackEnd
+
+# running backend
+ - npm install express 
+ - npm install multer 
+    - npm install --save @types/multer          ##solves error for req.file
+ - npm install csv-parser
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,6 +15,13 @@ import * as cors from "cors";
 import { Request, Response } from 'express';
 import * as auth from "../../auth";
 
+//imports for csv upload
+import multer from 'multer';
+import csvParser from 'csv-parser';
+import { Readable } from 'stream';
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 //initialize firebase in order to access its services
 admin.initializeApp(functions.config().firebase);
@@ -41,128 +48,128 @@ const reSPIRE = /^[0-9]{8}$/
  * user
  */
 export interface User {
-    email: string;
-    name: string;
-    /**
-     * 0 for user, 1 for leader, 2 for locker manager
-     */
-    permLvl: number;
-    phoneNum: number;
-    SPIRE_ID?: number;
-    /**
-     * false for expired, true for signed and valid
-     */
-    waiver: boolean;
-    [property: string]: any;
+  email: string;
+  name: string;
+  /**
+   * 0 for user, 1 for leader, 2 for locker manager
+   */
+  permLvl: number;
+  phoneNum: number;
+  SPIRE_ID?: number;
+  /**
+   * false for expired, true for signed and valid
+   */
+  waiver: boolean;
+  [property: string]: any;
 }
 
 /**
  * gear
  */
 export interface Gear {
-    /**
-     * The catagories this item falls under; sport, gender,size?
-     */
-    category: string[];
-    checkedOut: boolean;
-    gearFields: ExtraFields;
-    /**
-     * Tag: format is 3 letters 3 numbers
-     */
-    id: string;
-    leaderFields: LeaderFields;
-    managerFields: ManagerFields;
-    /**
-     * Generic name, ex: climbinghelmet
-     */
-    name: string;
-    /**
-     * Url to image
-     */
-    photo: string;
-    [property: string]: any;
+  /**
+   * The catagories this item falls under; sport, gender,size?
+   */
+  category: string[];
+  checkedOut: boolean;
+  gearFields: ExtraFields;
+  /**
+   * Tag: format is 3 letters 3 numbers
+   */
+  id: string;
+  leaderFields: LeaderFields;
+  managerFields: ManagerFields;
+  /**
+   * Generic name, ex: climbinghelmet
+   */
+  name: string;
+  /**
+   * Url to image
+   */
+  photo: string;
+  [property: string]: any;
 }
 
 /**
  * extraFields
  */
 export interface ExtraFields {
-    brand: string;
-    color: string;
-    notes?: string;
-    size?: string;
-    [property: string]: any;
+  brand: string;
+  color: string;
+  notes?: string;
+  size?: string;
+  [property: string]: any;
 }
 
 /**
  * leaderFields
  */
 export interface LeaderFields {
-    leaderNotes?: string;
-    /**
-     * can be anyone
-     */
-    whoHasThis: User;
-    /**
-     * must be a leader
-     */
-    whoLentThis: User;
-    [property: string]: any;
+  leaderNotes?: string;
+  /**
+   * can be anyone
+   */
+  whoHasThis: User;
+  /**
+   * must be a leader
+   */
+  whoLentThis: User;
+  [property: string]: any;
 }
 
 /**
  * managerFields
  */
 export interface ManagerFields {
-    "created (date)": string;
-    "lastUpdated (date)": string;
-    managerNotes: string;
-    price: number;
-    [property: string]: any;
+  "created (date)": string;
+  "lastUpdated (date)": string;
+  managerNotes: string;
+  price: number;
+  [property: string]: any;
 }
 
 // Create new user
 app.post('/api/createUser', async (req, res) => {
-    try {
-        const user: User = {
-            email: req.body['email'],
-            name: req.body['firstName'] + ' ' + req.body['lastName'], // Might have to be changed to only post the fullName
-            permLvl: req.body['permLevel'],
-            phoneNum: req.body['contactNumber'],
-            SPIRE_ID: req.body['id'],
-            waiver: req.body['waiver'],
-            ...req.body  // Include any additional properties sent by the client
-        }
-        auth.handleSignUp(user, user.email, req.body['password']);
-        const newDoc = await db.collection(userCollection).add(user);
-        res.status(201).send(`Created a new user: ${newDoc.id}`);
-    } catch (error) {
-        res.status(400).send(`User should contain email, name, permissionLevel, contactNumber, id, and waiver fields, along with any additional properties.`);
+  try {
+    const user: User = {
+      email: req.body['email'],
+      name: req.body['firstName'] + ' ' + req.body['lastName'], // Might have to be changed to only post the fullName
+      permLvl: req.body['permLevel'],
+      phoneNum: req.body['contactNumber'],
+      SPIRE_ID: req.body['id'],
+      waiver: req.body['waiver'],
+      ...req.body  // Include any additional properties sent by the client
     }
+    auth.handleSignUp(user, user.email, req.body['password']);
+    const newDoc = await db.collection(userCollection).add(user);
+    res.status(201).send(`Created a new user: ${newDoc.id}`);
+  } catch (error) {
+    res.status(400).send(`User should contain email, name, permissionLevel, contactNumber, id, and waiver fields, along with any additional properties.`);
+  }
 });
 
 // Get all users
 app.get('/api/getAllusers', async (req, res) => {
-    try {
-        const userQuerySnapshot = await db.collection(userCollection).get();
-        const users: any[] = [];
-        userQuerySnapshot.forEach(
-            (doc) => {
-                users.push({
-                    id: doc.id,
-                    data: doc.data()
-                });
-            }
-        );
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+    const userQuerySnapshot = await db.collection(userCollection).get();
+    const users: any[] = [];
+    userQuerySnapshot.forEach(
+      (doc) => {
+        users.push({
+          id: doc.id,
+          data: doc.data()
+        });
+      }
+    );
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // Get a single user by firebase ID
 app.get('/api/getUser', (req: Request, res: Response) => {
-  const userId: string | undefined = req.query.userId as string | undefined; 
+  const userId: string | undefined = req.query.userId as string | undefined;
   if (!userId) {
     return res.status(400).json({ error: 'userId parameter is required' });
   } else {
@@ -186,16 +193,16 @@ app.get('/api/getUserById', async (req: Request, res: Response) => {
     }
 
     if (reSPIRE.test(identifier)) {
-        const querySnapshot = await db.collection(userCollection)
-            .where('SPIRE_ID', '==', identifier)
-            .get();
-        
-        if (querySnapshot.empty) {
-            return res.status(404).send('User not found');
-        } else {
-            const user = querySnapshot.docs[0]; // Assuming there is only one matching user
-            return res.status(200).json({ id: user.id, data: user.data() });
-        }
+      const querySnapshot = await db.collection(userCollection)
+        .where('SPIRE_ID', '==', identifier)
+        .get();
+
+      if (querySnapshot.empty) {
+        return res.status(404).send('User not found');
+      } else {
+        const user = querySnapshot.docs[0]; // Assuming there is only one matching user
+        return res.status(200).json({ id: user.id, data: user.data() });
+      }
     } else if (reEmail.test(identifier)) {
       const querySnapshot = await db.collection(userCollection)
         .where('email', '==', identifier)
@@ -218,18 +225,46 @@ app.get('/api/getUserById', async (req: Request, res: Response) => {
 
 // Delete a user
 app.delete('/api/users/:userId', (req, res) => {
-    db.collection(userCollection).doc(req.params.userId).delete()
-        .then(() => res.status(204).send("Document successfully deleted!"))
-        .catch(function (error) {
-            res.status(500).send(error);
-        });
+  db.collection(userCollection).doc(req.params.userId).delete()
+    .then(() => res.status(204).send("Document successfully deleted!"))
+    .catch(function (error) {
+      res.status(500).send(error);
+    });
 })
 
 // Update a user
 app.put('/api/users/:userId', async (req, res) => {
-    await db.collection(userCollection).doc(req.params.userId).set(req.body,{merge:true})
-    .then(()=> res.json({id:req.params.userId}))
-    .catch((error)=> res.status(500).send(error))
+  await db.collection(userCollection).doc(req.params.userId).set(req.body, { merge: true })
+    .then(() => res.json({ id: req.params.userId }))
+    .catch((error) => res.status(500).send(error))
+});
+
+//upload csv file
+app.post('/api/upload-csv', upload.single('csvFile'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const csvBuffer = req.file.buffer;
+  const readStream = new Readable({
+    read() {
+      this.push(csvBuffer);
+      this.push(null);
+    }
+  });
+
+  const results: string[] = [];
+
+  readStream
+    .pipe(csvParser())
+    .on('data', (data: string) => results.push(data))
+    .on('end', () => {
+      // At this point, 'results' contains the parsed CSV content in JSON format.
+      console.log(results);
+      //res.status(200).json(results);
+    });
+
+  return res.status(200)
 });
 
 
